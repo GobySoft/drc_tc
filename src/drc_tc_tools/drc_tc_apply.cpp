620b91edfaa52669ce8439058499c472f5c77ac2
Toby Schneider <tes.aubergine@gmail.com>
2014-12-16 13:39:02 -0500

    Changed tbf buffer size for larger rates to allow these rates to be achieved. Changed from tc qdisc replace to tc qdisc delete / add which seems more reliable. Add tbf before netem if using

--- src/drc_tc_tools/drc_tc_apply.cpp
+++ src/drc_tc_tools/drc_tc_apply.cpp
@@ -34,22 +34,20 @@ drc_tc::DrcTcApply::DrcTcApply(DRCGUIConfig* cfg) :
     ApplicationBase(cfg),
     cfg_(*cfg)
 {
-    
-    {        
+    {
         std::stringstream netem_command;
-        netem_command << "/sbin/tc qdisc replace dev tun" << cfg_.tunnel_num()
-                      << " root handle 1:0 netem delay " << cfg_.latency_ms() << "ms"
-                      << " loss " << cfg_.drop_percentage() << "%";
-        
+        netem_command << "/sbin/tc qdisc delete dev tun" << cfg_.tunnel_num() << "  root";
         tc_system(netem_command);
     }
-
+    
+    
     if(cfg_.do_rate_control())
     {
         std::stringstream netem_command;
-        netem_command << "/sbin/tc qdisc replace dev tun" << cfg_.tunnel_num() << " parent 1:1 handle 10: tbf rate "
+        netem_command << "/sbin/tc qdisc add dev tun" << cfg_.tunnel_num() << "  root handle 1:0 tbf rate "
                       << cfg_.max_rate();
 
+        
         switch(cfg_.rate_unit())
         {
             case DRCGUIConfig::BYTES_PER_SEC: netem_command << "bps"; break;
@@ -59,16 +57,43 @@ drc_tc::DrcTcApply::DrcTcApply(DRCGUIConfig* cfg) :
             case DRCGUIConfig::MBITS_PER_SEC: netem_command << "mbit"; break;
         }
 
-        netem_command << " buffer 1600 limit 3000";
+        const int use_hz = 100;
+        netem_command << " buffer ";
+
+        int buffer_size = 0;
+        switch(cfg_.rate_unit())
+        {
+            case DRCGUIConfig::BYTES_PER_SEC: buffer_size = cfg_.max_rate() / use_hz; break;
+            case DRCGUIConfig::KBYTES_PER_SEC: buffer_size = 1000*cfg_.max_rate() / use_hz; break;
+            case DRCGUIConfig::MBYTES_PER_SEC: buffer_size =  1000000*cfg_.max_rate() / use_hz; break;
+            case DRCGUIConfig::KBITS_PER_SEC: buffer_size =  1000/8*cfg_.max_rate() / use_hz; break;
+            case DRCGUIConfig::MBITS_PER_SEC: buffer_size =  1000000/8*cfg_.max_rate() / use_hz; break;
+        }
+        if(buffer_size < 1600)
+            buffer_size = 1600;
+        
+        netem_command << buffer_size;
+        netem_command << " latency 10ms";
         
         tc_system(netem_command);
     }
-    else
+
+    std::string hierarchy = " root handle 1:0";
+    if(cfg_.do_rate_control())
     {
+        hierarchy = " parent 1:1 handle 10:";
+    }
+
+    if(cfg_.latency_ms() > 0 || cfg_.drop_percentage() > 0)
+    {            
         std::stringstream netem_command;
-        netem_command << "/sbin/tc qdisc replace dev tun" << cfg_.tunnel_num() << " parent 1:1 pfifo";
+        netem_command << "/sbin/tc qdisc add dev tun" << cfg_.tunnel_num()
+                      << hierarchy << " netem delay " << cfg_.latency_ms() << "ms"
+                      << " loss " << cfg_.drop_percentage() << "%";
         tc_system(netem_command);
     }
+
+
     
     quit();
 }
